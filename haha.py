b8de26411c38b76180855abd82ce45abddd31325
Leo <502008192@qq.com>
2019-09-08 16:49:09 +0800

    accept current

--- haha.py
+++ haha.py
@@ -6,10 +6,7 @@ a = tf.constant(1)
 print(a)
 print("first commit")
 print("first commit on dev")
-<<<<<<< HEAD
 print("add in master")
 print("add in master")
 print("add in dev")
-=======
-print("add in master")
->>>>>>> 69877afdbf23966989b4ede6d689074766530c45
+print("add1 in dev")
